68c43bd0ab348d4c9ccb7f25b190d853ec478e91
pjt3591oo <pjt3591oo@gmail.com>
2019-09-07 14:11:05 +0900

    etc에 ascii art를 추가한다

---
+++ etc/ascii_art1.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# https://manytools.org/hacker-tools/convert-images-to-ascii-art/go 
+
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+echo -n $'\033[01;32m'
+echo "MMMMMMMMMMMMMMMWXXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMWKd:,,'':o0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0dc;;:lkNMMMMMMMMMMMMM"
+echo "MMMMMMMMMMNx'.,cooc,.'dNMMMMMMMMMMMMMMMMMMMMMMMMMMW0:..,cc;'.;kWMMMMMMMMMMM"
+echo "MMMMMMMMMNo.,dO0000Ox;.cXMMMMMMMMMMMMMMMMMMMMMMMMWk'.cxO00Oko..oXMMMMMMMMMM"
+echo "MMMMMMMMNo.'x0OO00OO0x,.cXMMMMMMMMMMMMMMMMMMMMMMWO''oO00000OOd..lNMMMMMMMMM"
+echo "MMMMMMMWx..oO00O000000d..dWMMMMMMMMMMMMMMMMMMMMMX:.cO000O0000Oo..xWMMMMMMMM"
+echo "MMMMMMMX: ;k0000000000Oc ,0MMMMMMMMMMMMMMMMMMMMMk..o00OkddxOO0k; :XMMMMMMMM"
+echo "MMMMMMMO..lO0OkolldkOO0d..xMMMMMMMMMMMMMMMMMMMMNl ,k0x;.  .'lOOl.'0MMMMMMMM"
+echo "MMMMMMMk..d0x:.   ..ck0x'.dWMMMMWWWWWWWWWWWWMMMNc ;kk,      .oOo..OMMMMMMMM"
+echo "MMMMMMMk..dOc       .oOd. ;dolcc:;,,,,,,,,;:cclc'.cOk;      .dOl.'OMMMMMMMM"
+echo "MMMMMMM0,.cOx;.    .:kOo,'''',;;:cllllllllc:;;;,';d00kc,..';dOk: :XMMMMMMMM"
+echo "MMMMMMMNl.,k0OxlccoxOOOOOOOOOO0O000000000000000OOOO000OOkxOO0Oo..dWMMMMMMMM"
+echo "MMMMMMMMO'.cO00000000OO0000000O0000000000O00000000000O0000000Ol..l0WMMMMMMM"
+echo "MMMMMMNOc..lO00000000O000O00000000000000000000000000O0000000OOkl,..;dKWMMMM"
+echo "MMMMNk;..:dO0000000000000000000000000000000O0000000000000000O00OOxl,.'dNMMM"
+echo "MMWKc..ckO000000000000000000000000000000000000000000000000000000000Od,.c0WM"
+echo "MMO,.;xO0O0000000000000000000000000000000000000000000000000000000OO00Ol.'kW"
+echo "MNl.;k00O0000000000000000000000000000000000000000000000000000000000000k:.,0"
+echo "M0,'x0O000O000000000000000000000000000000000000000000O000O0000000000000x' l"
+echo "Mx.;k00000000KKK00K00000000000000000000000000000000000000000KKK0000K000k; :"
+echo "M0,'d00000K000KK00K0000000000000000000000000000000000000000K0000000K000x'.l"
+echo "MNl.;k0000K000KKK00000000000000000000000000000000000000O00000000K000000d.;0"
+echo "MM0;.;xO00KK000000000OOOO0000000000000000000000000000OOOO00K0K0000000OxlcOW"
+echo "MMMKl..cxO00000000O00Oo,;lxOO000O0000000000000000Okdl;,lkO000000000Ox;'oKWM"
+echo "MMMMWOc..;ok000000000Oxc,..':okOO000000000000Okdl;...,cdOO00O0Oxddoc,,xNMMM"
+echo "MMMMMMW0o,.':dkO0000000OOxo:'..;coxkkOOOOkdl:,...,:okO0O0000Okd;...ckXMMMMM"
+echo "MMMMMMMMMN0l,.',:oxkO000O00Okdl;'..''''''...,;ldkO00000OOxdc;'.'cxKWMMMMMMM"
+echo "MMMMMMMMMMMWNOdc;'..';cldxxkOO0OkxoollllloxkOOOOkkxdlc:,'..,:dOXWMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMWX0xo:;''..'',,,;:cccccccc::,,,''..'',:ldkKNWMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMWWXK0kkxoooc::::::::::cloodxkOKXWWMMMMMMMMMMMMMMMMMMMM"
+echo "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+
+echo 
+
+echo "                  .,.                                                        "
+echo "             *@@@@@@@@*                              *&@@@@@#,              "
+echo "           .%@%*    *%@%                           *@@#*   /%@#             "
+echo "          .&@*        *@&.                        *@%        (@&,           "
+echo "         .&@(          (@&                       ,@#          (@&.          "
+echo "         (@%            %@(                      %@(           %@#          "
+echo "         &@/            /@&                     ,@@*   *(#/    /@&          "
+echo "        *@&    *#&&(*    &@,                    (@%  *@@@@@@%  *&@,         "
+echo "        *@&  /@@@@@@@%   &@,                    (@%  &@@@@@@@( *&@,         "
+echo "        *@&  #@@@@@@@@* *&@&&&@@@@@@@@@@@@@@@@@@@@/  *@@@@@@&  *@@,         "
+echo "         &@/  /&@@@@%*  *//(//*                *//     *#%(*   (@%          "
+echo "         /@%                                                  *@@*          "
+echo "          %@#                                                 *%@&/         "
+echo "       .%@@%                                                    *#@@@*      "
+echo "     .%@@/                                                         *%@&.    "
+echo "    /@@/                                                             *%@/   "
+echo "   (@&*                                                                %@#  "
+echo "   &&*                                                                 *&@/ "
+echo "  *@(                                                                   /@& "
+echo "  *@(                                                                   /@& "
+echo "  .&&                                                                   %@/ "
+echo "   (@%                                                                  %%  "
+echo "    #@&*                                                             /((   "
+echo "     ,&@&*             #@@%/                       (&@@&*          ,*&@*    "
+echo "       ,&@@#*            /%@@@#*              *(&@@@#*         /#(%&@(      "
+echo "          /@@@(*             (&@@/     *#%@@@@#*            (&@@%,        "
+echo "            .(&@@@#/*           *(&@@@@@@@/            *(&@@&(.           "
+echo "                 *#&@@@&%((**                   **/(%&@@@@%(.               "
+echo "                      .*(%&@@@@@@@@@@@@@@@@@@@@@@@@%(*.                     "
+echo "                                  ..........                             "
+                                                                                                                               
+
+echo -n $'\033[01;32m'
+echo $''
+echo $'      _,.'
+echo $'    ,` -.)'
+echo $'   \'( _/\'-\\\\-.'
+echo $'  /,|`--._,-^|          ,'
+echo $'  \\_| |`-._/||          ,\'|'
+echo $'    |  `-, / |         /  /'
+echo $'    |     || |        /  /'
+echo $'     `r-._||/   __   /  /'
+echo $' __,-<_     )`-/  `./  /'
+echo $'\'  \\   `---\'   \\   /  /'
+echo $'    |           |./  /'
+echo $'    /           //  /'
+echo $'\\_/\' \\         |/  /'
+echo $' |    |   _,^-\'/  /'
+echo $' |    , ``  (\\/  /_'
+echo $'  \\,.->._    \\X-=/^'
+echo $'  (  /   `-._//^`'
+echo $'   `Y-.____(__}'
+echo $'    |     {__)'
+echo $'          ()`'
+
+echo
+
+echo -n $'\033[01;33m'
+echo "##         ######   ###   ##  ##    ##  ##    ## "
+echo "##         ######   ###   ##  ##    ##  :##  ##: "
+echo "##           ##     ###:  ##  ##    ##   ##  ##  "
+echo "##           ##     ####  ##  ##    ##   :####:  "
+echo "##           ##     ##:#: ##  ##    ##    ####   "
+echo "##           ##     ## ## ##  ##    ##    :##:   "
+echo "##           ##     ## ## ##  ##    ##    :##:   "
+echo "##           ##     ## :#:##  ##    ##    ####   "
+echo "##           ##     ##  ####  ##    ##   :####:  "
+echo "##           ##     ##  :###  ##    ##   ##::##  "
+echo "########   ######   ##   ###  :######:  :##  ##: "
+echo "########   ######   ##   ###   :####:   ##    ## "
+
+echo
+
--- etc/progressbar.sh
+++ etc/progressbar.sh
@@ -4,6 +4,17 @@
 #    $1 ticks: 총 틱 갯수     (default 10)
 #    $2 delay: 한 틱당 소요시간 (default 0.3)
 ####################################
+
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
 progress() {
   local ticks=$1 
   local delay=$2 
@@ -20,9 +31,10 @@ progress() {
   fi
 
   for ((i = 1 ; i <= $ticks ; i++)); do
-    local w=80 p=$i;  shift
-    printf -v dots "%*s" "$(( $p*$w/$ticks ))" ""; dots=${dots// /.};
-    printf "\r\e[K|%-*s| %3d(s) / %s(s) %s" "$w" "$dots" "$p" "$ticks" "still working..."; 
+    local w=80 p=$i;  
+    printf -v dots "%*s" "$(( $p*$w/$ticks ))" ""; 
+    dots=${dots// /.};
+    printf "${GREEN}\r\e[K|%-*s| %3d(s) / %s(s) %s" "$w" "$dots" "$p" "$ticks" "still working..."; 
     sleep $delay   
   done ; echo
 }
