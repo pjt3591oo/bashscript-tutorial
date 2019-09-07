ccc657f60c2fbb8b4cad5d707543a08d41eff7d7
pjt3591oo <pjt3591oo@gmail.com>
2019-09-08 07:36:11 +0900

    templete 파일 기반에서 설정 파일 만드는 쉘 스크립트 작성중

--- etc/templete.sh
+++ etc/templete.sh
@@ -1,6 +1,30 @@
-set -x
+################################
+# 템플릿 파일을 기반으로 문자열 치환후 새로운 파일을 생성한다
+# params
+#     - $1: 치환대상
+#     - $2: 치환 값
+#     - $3: 템플릿 파일 경로
+#     - $4: 결과파일 경로
+################################
+function createFromTemplete(){
+  
+  local search=$1
+  local replace=$2
 
-search="\$\$[account]"
-replace="0x123"
+  local ORIGIN_FILE_PATH=$3
+  local RESULT_FILE_PATH=$4
 
-sed -i "s/${search}/${replace}/g" templete.txt
+  sed "s/${search}/${replace}/g" $ORIGIN_FILE_PATH > $RESULT_FILE_PATH
+}
+
+
+saerchs=("+ACCOUNT1" "+ACCOUNT2")
+replaces=("0x123123", "0x234")
+
+for i in "${!saerchs[@]}"; do 
+  printf "$i " " ${saerchs[$i]}"
+  createFromTemplete ${saerchs[$i]} ${replaces[$i]} "./templete_node.toml" "./node.toml"
+done
+
+
+createFromTemplete $saerchs $replaces "./templete_node.toml" "./node.toml"
